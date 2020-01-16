943012ef949551f90b0d7d4ccff841956b694300
dtitov <dmytro.titov@gmail.com>
2020-01-16 10:19:21 +0100

    Finalize requests test

--- requests/requests_test.go
+++ requests/requests_test.go
@@ -1,9 +1,14 @@
 package requests
 
 import (
+	"encoding/json"
+	"github.com/logrusorgru/aurora"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -20,22 +25,48 @@ func TestMain(m *testing.M) {
 
 func setup() {
 	server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		auth, password, ok := r.BasicAuth()
-		println(auth)
-		println(password)
-		println(ok)
-		w.WriteHeader(200)
+		response := make(map[string]string)
+		auth, password, _ := r.BasicAuth()
+		response["auth"] = auth
+		response["pass"] = password
+		response["header"] = r.Header.Get("header")
+		response["params"] = r.URL.RawQuery
+		bytes, err := json.Marshal(response)
+		if err != nil {
+			log.Fatal(aurora.Red(err))
+		}
+		_, err = w.Write(bytes)
+		if err != nil {
+			log.Fatal(aurora.Red(err))
+		}
 	}))
 	client = NewClient(server.Client())
 	url = server.URL
 }
 
 func TestDoRequest(t *testing.T) {
-	get, err := client.DoRequest(http.MethodGet, url, nil, nil, nil, nil, nil)
+	username := "username"
+	password := "password"
+	get, err := client.DoRequest(http.MethodGet,
+		url,
+		strings.NewReader("Body"),
+		map[string]string{"header": "test header value"},
+		map[string]string{"param": "test param value"},
+		&username,
+		&password)
 	if err != nil {
+		t.Error(err)
+	}
+	body := get.Body
+	defer body.Close()
+	bytes, err := ioutil.ReadAll(body)
+	if err != nil {
+		t.Error(err)
+	}
+	response := string(bytes)
+	if response != `{"auth":"username","header":"test header value","params":"param=test+param+value","pass":"password"}` {
 		t.Error()
 	}
-	println(get)
 }
 
 func teardown() {
